aa5054feea88820e2ad7e9896e85f6afc832b87c
Lucas Gerez <gerezlucas003@gmail.com>
2026-06-30 20:40:34 -0300

    entregando proyecto parcial 2

--- src/segundoparcialprogramacion2/Main.java
+++ src/segundoparcialprogramacion2/Main.java
@@ -6,11 +6,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //--------------------------------------------
         Scanner scanner = new Scanner(System.in);
         Biblioteca biblioteca = new Biblioteca();
+        //--------------------------------------------
         cargarDatosDePrueba(biblioteca);
         
-        
+        //--------------------------------------------
         Menu menu = new Menu(scanner, biblioteca);
         menu.iniciar();
         
